c41d6f7077e39d83899c81aff240f9b2a803b04e
Your Name <khorenterhovhannisyan@gmail.com>
2020-02-26 13:09:59 +0400

    added hash for sign up

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const companyRouter = require('./api/routes/companies.routes');
 const userRouter = require('./api/routes/users.routes');
+const cors = require("cors")
 
 const config = require('./config');
 
@@ -12,11 +13,12 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan('combined'));
+app.use(cors());
 
 app.use(companyRouter);
 app.use(userRouter);
-
-mongoose.connect(config.db.url, { useNewUrlParser: true }, () => {
+//ymongoose.set('useCreateIndex', true);
+mongoose.connect(config.db.url, { useNewUrlParser: true ,useUnifiedTopology: true}, () => {
   app.listen(config.server.port, () => {
     console.log(`Magic is happening on port ${config.server.port}`);
   });
