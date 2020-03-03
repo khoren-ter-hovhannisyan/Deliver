ea4d122690735c024fe339cf0adfd7541216cd8c
Ani <anigevorgyan0@gmail.com>
2020-03-03 15:31:06 +0400

    add: socket for admin dashboard

--- src/server.js
+++ src/server.js
@@ -6,11 +6,17 @@ const mongoose = require('mongoose');
 const companyRouter = require('./api/routes/companies.routes');
 const userRouter = require('./api/routes/users.routes');
 const loginRouter = require('./api/routes/login.routes');
-const cors = require("cors")
+const cors = require("cors");
+const socket = require('socket.io');
 
+const handlers = require('./api/handlers/users.handlers');
 const config = require('./config');
 
 const app = express();
+const server = app.listen(config.server.port, () => {
+  console.log(`Magic is happening on port ${config.server.port}`);
+});
+
 
 app.use(express.json());
 app.use(morgan('combined'));
@@ -20,8 +26,23 @@ app.use(userRouter);
 app.use(companyRouter);
 app.use(loginRouter);
 
-mongoose.connect(config.db.url, { useNewUrlParser: true ,useUnifiedTopology: true}, () => {
-  app.listen(config.server.port, () => {
-    console.log(`Magic is happening on port ${config.server.port}`);
+const io = socket(server);
+
+mongoose.connect(config.db.url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}, () => {
+  server
+});
+
+io.on('connection', (socket) => {
+  console.log('Socket magic is happening', socket.id);
+  socket.on('new_user', (userData) => {
+    socket.broadcast.emit('update_user_list', userData);
+    console.log('getting all users', userData);
   });
+  socket.on('new_company', (companyData) => {
+    socket.broadcast.emit('update_company_list', companyData);
+  })
 });
