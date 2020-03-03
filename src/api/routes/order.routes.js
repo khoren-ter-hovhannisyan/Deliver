3cdc808d7d2b4ee07be6a053f6bbaaed4705bebf
Hayk <hayk_1998@yahoo.com>
2020-03-03 18:14:36 +0400

    feat :add 2 order reqs

--- src/api/routes/order.routes.js
+++ src/api/routes/order.routes.js
@@ -1,10 +1,15 @@
 const { Router } = require('express')
 
-const { createOrder, getAllActiveOrder } = require('../handlers/order.hendlers')
+const {
+  createOrder,
+  getAllActiveOrder,
+  getCompanyOrders,
+} = require('../handlers/order.hendlers')
 
 router = Router()
 
 router.post('/create-order', createOrder)
 router.get('/orders', getAllActiveOrder)
+router.get('/company-orders/:id', getCompanyOrders)
 
 module.exports = router
