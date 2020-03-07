ae7af2c98a9d2281bfa3204c58a96153f0d0dab1
Hayk <hayk_1998@yahoo.com>
2020-03-07 14:18:23 +0400

    fix: router order enpoint names

--- src/api/routes/order.routes.js
+++ src/api/routes/order.routes.js
@@ -11,11 +11,11 @@ const {
 
 router = Router()
 
-router.post('/create-order', createOrder)
-router.get('/orders', getAllActiveOrder)
-router.get('/company-orders/:id', getCompanyOrders)
-router.get('/user-orders/:id', getUserOrders)
+router.post('/orders', createOrder)
 router.delete('/orders/:id', delOrder)
 router.put('/orders/:id', updateOrder)
+router.get('/active-orders', getAllActiveOrder)
+router.get('/company-orders/:id', getCompanyOrders)
+router.get('/user-orders/:id', getUserOrders)
 
 module.exports = router
